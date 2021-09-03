c0f62e267811309a4d969d2a9c7ce6a57c86b086
Himanshu Tripathi <heman.tri2208@gmail.com>
2021-09-03 23:57:02 +0530

    Added basic Javascript

---
+++ app.js
@@ -0,0 +1,19 @@
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-input");
+const todoList = document.querySelector(".todo-input");
+
+//Event Listeners
+todoButton.addEventListener("click", addTodo);
+
+//Functions
+function addTodo() {
+    //preventing form from Submitting
+    event.preventDefault();
+
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    //Create LI
+
+}
